7bfafe42c38a4079c02a3bae53ae078d58393318
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-23 17:05:36 +0300

    init android

---
+++ app/src/main/kotlin/test/android/kiosk/App.kt
@@ -0,0 +1,9 @@
+package test.android.kiosk
+
+import android.app.Application
+
+internal class App : Application() {
+    override fun onCreate() {
+        // todo
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "KioskSample"
+
+include("app")
