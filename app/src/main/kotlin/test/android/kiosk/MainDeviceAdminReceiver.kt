23224b6d29b8ae3e653d0720c48a2f9938c2e413
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-23 17:09:50 +0300

    DeviceAdminReceiver

---
+++ app/src/main/kotlin/test/android/kiosk/MainDeviceAdminReceiver.kt
@@ -0,0 +1,11 @@
+package test.android.kiosk
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+internal class MainDeviceAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        // todo
+    }
+}
